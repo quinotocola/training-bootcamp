50ff8634a2f56d647b3b96643d625b1041df3f17
quinotocola <quinotoco@gmail.com>
2022-10-20 19:31:04 -0400

    agregando from intelliJ

---
+++ untitled/src/main/java/client.java
@@ -0,0 +1,13 @@
+public class client {
+    public static void main (String []asda){
+        int a=7;
+        int b=8;
+        if (a>b){
+            a=a*2;
+            System.out.println(a);
+        } else {
+            b=b*2;
+            System.out.println(b);
+        }
+    }
+}
